de620e5d61f4e9fa6e6596ad09952ff7aa2dd87d
Kireeti Katakam <kireetikatakam@macbook-pro-4.lan>
2024-02-05 20:39:43 -0600

    adding internet gateway and route table for internet access to public subnets

--- main.tf
+++ main.tf
@@ -30,3 +30,22 @@ resource "aws_subnet" "private" {
   cidr_block        = var.private_subnet_cidr_blocks[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
